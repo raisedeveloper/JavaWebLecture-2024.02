216a244849263aceb13fe5f0480f9b76bbc8ffec
raisedeveloper <ilove4903@naver.com>
2024-02-13 16:04:52 +0900

    Servlet

--- src/main/java/ch06_basic/Ex02_Parameter.java
+++ src/main/java/ch06_basic/Ex02_Parameter.java
@@ -8,23 +8,29 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //localhost:8080/jw/ch06/params?id=101&title=title  == 주소창에 입력
+//localhost:8080/jw/ch06/params?id=&title=  == 입력 값은 ""
+//localhost:8080/jw/ch06/params  == 입력 값은 null
 @WebServlet("/ch06/params")
 public class Ex02_Parameter extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public Ex02_Parameter() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		String id_ = req.getParameter("id");
-		int id = Integer.parseInt(id_);
+//		int id = Integer.parseInt(id_);		// id 값이 안들어오면 예외 발생
+		int id = 0;		//입력이 안됬을 때 사용하는 기본값
+		if(id_ !=null && ! id_.equals("")) 	// 이 두가지 경우가 다 아니어야 id = Integer.parseInt(id_); 가 됨
+			id = Integer.parseInt(id_);	
+			
 		String title = req.getParameter("title");
-		System.out.println("id: " + id + ", title: " + title);
+		System.out.println("Get id: " + id + ", title: " + title);
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String id_ = req.getParameter("id");
+		int id = 0;		//입력이 안됬을 때 사용하는 기본값
+		if(id_ !=null && ! id_.equals(""))
+			id = Integer.parseInt(id_);	
+			
+		String title = req.getParameter("title");
+		System.out.println("Post id: " + id + ", title: " + title);
 	}
 }
